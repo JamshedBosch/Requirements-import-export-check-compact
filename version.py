b03e535aa452d5f09958d5e4a9122fc86611c3c9
JamshedMalik <malik.iqbal@hotmail.de>
2026-01-26 12:15:18 +0100

    Update version to 4.1.0 in version.py for release.

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 """Version information for the Requirements Import Export Checker."""
 
-__version__ = '4.0.12'
+__version__ = '4.1.0'
 __company__ = 'Robert Bosch GmbH'
 __product_name__ = 'Requirements Import Export Checker' 
